8c1df6927162b042c28f971c5a92b22da2dfec15
John Braithwaite <jwbraith@gmail.com>
2020-12-22 03:06:36 -0800

    got sketch centering and drawing

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  var cnv = createCanvas(420, 420);
+  cnv.parent('sketch');
+  background(0);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  rect(width / 2, height / 2, 42, 32);
+}
