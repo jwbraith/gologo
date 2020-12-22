034e6da67a094842e84ae5d16d208775b34f3c1e
John Braithwaite <jwbraith@gmail.com>
2020-12-22 03:57:11 -0800

    keyboard controls

--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
+let x, y;
+let delta = 5;
 
 function setup() {
-  var cnv = createCanvas(420, 420);
+  pixelDensity(1);
+  var cnv = createCanvas(300, 300);
   cnv.parent('sketch');
   background(0);
   rectMode(CENTER);
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
   background(0);
   stroke(255);
-  rect(width / 2, height / 2, 42, 32);
+  rect(x, y, 1, 1);
+  if (y > 0 && keyIsDown(UP_ARROW)) {
+    y -= delta;
+  }
+  if (y < height && keyIsDown(DOWN_ARROW)) {
+    y += delta;
+  }
+  if (x > 0 && keyIsDown(LEFT_ARROW)) {
+    x -= delta;
+  }
+  if (x < width && keyIsDown(RIGHT_ARROW)) {
+    x += delta;
+  }
 }
